804fdb5bb96e72253e00ac27ae704160724ccae5
Edoxy <gonnellino@gmail.com>
2021-04-09 21:48:12 +0200

    prova algoritmo horner

---
+++ Polinomi/horner.py
@@ -0,0 +1,29 @@
+import numpy as np
+import time
+
+
+def horner(coef, value):
+    t1 = time.perf_counter()
+    result = 0
+    for i in range(len(coef)):
+        result = result * value + coef[i]
+    t2 = time.perf_counter()
+    print('Horner execution time : ', t2-t1)
+    return result
+
+def normal(coef, value):
+    t1 = time.perf_counter()
+    result = 0
+    for i in range(len(coef)):
+        result = result + coef[i] * (value**i)
+        t2 = time.perf_counter()
+    print('Normal execution time : ', t2-t1)
+    return result
+
+if __name__ == '__main__':
+    value = 1
+    coef = [i-100 for i in range(1000)]
+    print(horner(coef, value))
+    print(normal(coef, value))
+
+    
